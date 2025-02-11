3784b881136a89a2f137f9072741d1d598091aab
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-02-11 19:48:36 +0300

    feat: added connect wallet button in builder

--- src/components/Builder/BuilderButton.tsx
+++ src/components/Builder/BuilderButton.tsx
@@ -1,10 +1,27 @@
+import { FancyButton } from '../FancyButton'
 import { CreateButton } from './CreateButton'
 import { UpdateButton } from './UpdateButton'
+import { useWallet } from '@solana/wallet-adapter-react'
+import { useWalletModal } from '@solana/wallet-adapter-react-ui'
 import { useLocation } from 'react-router-dom'
 
 export const BuilderButton = () => {
+  const { connected } = useWallet()
+  const { setVisible } = useWalletModal()
+
   const { pathname } = useLocation()
   const isCreator = pathname === '/create'
+
+  if (!connected) {
+    return (
+      <FancyButton
+        onClick={() => setVisible(true)}
+        className='mb-4 w-full max-w-64 self-center'
+      >
+        Connect Wallet
+      </FancyButton>
+    )
+  }
 
   if (isCreator) return <CreateButton />
   return <UpdateButton />
