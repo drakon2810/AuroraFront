ed185638c7ac8e34d246f886485d0be51e2de9e2
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-02-13 16:15:06 +0300

    fix: website item card

--- src/components/Account/WebsitesItem.tsx
+++ src/components/Account/WebsitesItem.tsx
@@ -42,33 +42,35 @@ export const WebsitesItem: FC<WebsitesItemsProps> = ({
   return (
     <li className='w-full'>
       <Card className='flex w-full flex-row items-center justify-between gap-4 p-4'>
-        <div className='flex items-center gap-4'>
-          <img
-            src={src || ImagePlaceholder}
-            className='h-12 w-12 rounded-full bg-neutral-500'
-          />
-          <div className='flex flex-col'>
-            <h4 className='flex gap-2 text-2xl leading-none'>
-              <span>{name}</span>
-              <a
-                href={websiteURL}
-                target='_blank'
-                className='text-neutral-400 transition-colors duration-300 hover:text-black dark:hover:text-white'
-              >
-                <ExternalLink size={18} />
-              </a>
-            </h4>
-            <span className='text-sm text-neutral-400'>{template}</span>
+        <div className='flex items-center justify-between gap-4'>
+          <div className='flex items-center gap-4'>
+            <img
+              src={src || ImagePlaceholder}
+              className='h-12 w-12 rounded-full bg-neutral-500'
+            />
+            <div className='flex flex-col'>
+              <h4 className='flex gap-2 text-2xl leading-none'>
+                <span>{name}</span>
+                <a
+                  href={websiteURL}
+                  target='_blank'
+                  className='text-neutral-400 transition-colors duration-300 hover:text-black dark:hover:text-white'
+                >
+                  <ExternalLink size={18} />
+                </a>
+              </h4>
+              <span className='text-sm text-neutral-400'>{template}</span>
+            </div>
           </div>
-        </div>
-        <div className='flex items-center gap-4'>
-          <Button asChild>
-            <Link to={`/edit?name=${name}&template=${template}`}>
-              {t('account.editButton')}
-            </Link>
-          </Button>
-          <ChangeDomain name={name} />
-          <DeleteWebsite name={name} />
+          <div className='flex items-center gap-4'>
+            <Button asChild>
+              <Link to={`/edit?name=${name}&template=${template}`}>
+                {t('account.editButton')}
+              </Link>
+            </Button>
+            <ChangeDomain name={name} />
+            <DeleteWebsite name={name} />
+          </div>
         </div>
       </Card>
     </li>
