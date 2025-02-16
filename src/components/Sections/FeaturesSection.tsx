e6818cb919f54f8e69eabfbb897d42e36820936c
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-02-16 22:20:11 +0300

    fix: features section paddings

--- src/components/Sections/FeaturesSection.tsx
+++ src/components/Sections/FeaturesSection.tsx
@@ -1,15 +1,16 @@
 import { Badge } from '../Badge'
 import { Description } from '../Description'
 import { FeaturesCards } from '../Features/FeaturesCards'
+import { Section } from './Section'
 import { useTranslation } from 'react-i18next'
 
 export const FeaturesSection = () => {
   const { t } = useTranslation()
 
   return (
-    <section
+    <Section
       id='features'
-      className='container flex flex-col items-center justify-center gap-12 pb-24'
+      className='flex flex-col items-center justify-center gap-12 pb-24'
     >
       <div className='container flex flex-col items-center justify-center gap-4 text-center'>
         <Badge>{t('home.features.badge')}</Badge>
@@ -17,6 +18,6 @@ export const FeaturesSection = () => {
         <Description>{t('home.features.description')}</Description>
       </div>
       <FeaturesCards />
-    </section>
+    </Section>
   )
 }
