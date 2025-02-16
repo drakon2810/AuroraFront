681d96c305b23323f837203843548d70c59795ae
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-02-16 21:49:45 +0300

    fix: recently created marquee blackout color

--- src/components/RecentlyCreatedMarquee/RecentlyCreatedMarquee.tsx
+++ src/components/RecentlyCreatedMarquee/RecentlyCreatedMarquee.tsx
@@ -39,8 +39,8 @@ export const RecentlyCreatedMarquee = () => {
           />
         ))}
       </Marquee>
-      <div className='pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-neutral-900'></div>
-      <div className='pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-neutral-900'></div>
+      <div className='pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-white dark:from-neutral-900'></div>
+      <div className='pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-white dark:from-neutral-900'></div>
     </div>
   )
 }
