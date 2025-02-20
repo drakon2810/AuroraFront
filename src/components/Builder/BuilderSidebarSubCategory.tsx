71db816d98ebf4e937b64a2703caba3ede57d215
Владислав <vladislav@MacBook-Air-Vladislav.local>
2025-02-20 20:06:20 +0300

    feat: added collapsible menu for editor

--- src/components/Builder/BuilderSidebarSubCategory.tsx
+++ src/components/Builder/BuilderSidebarSubCategory.tsx
@@ -1,9 +1,14 @@
-import { BuilderSidebarSubCategoryMenu } from './BuilderSidebarSubCategoryMenu'
+import {
+  Collapsible,
+  CollapsibleTrigger,
+  CollapsibleContent
+} from '../ui/collapsible'
+import { BuilderSidebarSubCategoryItem } from './BuilderSidebarSubCategoryItem'
 import { TemplateContext } from '@/contexts/TemplateContext'
 import { TemplateContextValues } from '@/types/contexts'
 import { TemplateSubCategoryField } from '@/types/templates'
 import { MoveRight } from 'lucide-react'
-import { FC, useContext, useState } from 'react'
+import { FC, useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 
 interface BuilderSidebarSubCategoryProps {
@@ -20,25 +25,21 @@ export const BuilderSidebarSubCategory: FC<BuilderSidebarSubCategoryProps> = ({
   ) as TemplateContextValues
   const { t } = useTranslation()
 
-  const [isActive, setIsActive] = useState(false)
-
   return (
-    <>
-      <button
-        onClick={() => setIsActive(true)}
-        className='flex w-full items-center justify-between gap-2 rounded-md p-3 text-start transition-colors duration-300 hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black'
-      >
+    <Collapsible>
+      <CollapsibleTrigger className='flex w-full items-center justify-between gap-2 rounded-md p-3 text-start transition-colors duration-300 hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black'>
         <span className='font-arial-black'>
           {t(`builder.${selectedTemplate}.categories.${label}`)}
         </span>
         <MoveRight size={16} />
-      </button>
-      <BuilderSidebarSubCategoryMenu
-        isActive={isActive}
-        setIsActive={setIsActive}
-        fields={fields}
-        label={label}
-      />
-    </>
+      </CollapsibleTrigger>
+      <CollapsibleContent>
+        <div className='flex flex-auto flex-col gap-4 rounded-md py-2 pl-8'>
+          {fields.map((field) => (
+            <BuilderSidebarSubCategoryItem key={field.name} {...field} />
+          ))}
+        </div>
+      </CollapsibleContent>
+    </Collapsible>
   )
 }
